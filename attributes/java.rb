1e1a0926cc78a7514a54b194128d2c4197e615f7
tomasz.michalak <tomek.michalak@gmail.com>
2019-01-16 11:23:29 +0100

    Fixing java package url location, changing some default settings in jenkins.yaml.

--- attributes/java.rb
+++ attributes/java.rb
@@ -1,3 +1,6 @@
 default['java']['jdk_version'] = '8'
 default['java']['install_flavor'] = 'oracle'
 default['java']['oracle']['accept_oracle_download_terms'] = true
+
+default['java']['jdk']['8']['x86_64']['url'] = 'https://s3-us-west-2.amazonaws.com/vagifzeynalov-chef/jdk-8u171-linux-x64.tar.gz'
+default['java']['jdk']['8']['x86_64']['checksum'] = 'b6dd2837efaaec4109b36cfbb94a774db100029f98b0d78be68c27bec0275982'
